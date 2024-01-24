9165f3cab5d4ef2a005bf0bd3be780af3c16151b
ABUTAREKALANGARI <AALANGAR@STEVENS.EDU>
2024-01-24 15:15:02 +0300

    The code is completed

--- answer.py
+++ answer.py
@@ -2,26 +2,24 @@ def hello_world():
     # Write a doc string below. Doc strings can be used to provide the user with a
     # description of what the function does.
     """
-    prints 'Hello, World' to the console.
+    This code prints 'Hello, World' to the console.
     """
 
     # Write a comment; comments are very useful to other programmers.
+    # This is the first code for me.
     # This is a simple print statement to display a greeting message
     # Write a Python statement to print Hello, World!
     print("Hello, World")
 
 def add(a,b):
     """
-    Adds two input values and returns the result.
-    parameters:
-    a (int or float): The first  operand
-    b (int or float): The second operand
-    
-    Returns:
-    int or float: The result of adding a and b
+    This code is to add to variables and return the result.
     """
 
     # Write simple code to add variable a and b
-    output = a + b
+    a=int(input("a="))
+    b=int(input("b="))
+    c=a+b
+    print("Ans=",c)
 
     return output
